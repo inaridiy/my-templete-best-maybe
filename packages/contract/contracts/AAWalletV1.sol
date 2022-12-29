2fb2232fed6a47c2e09bcee478499aec18941338
Inaridiy <inainainariinainari@gmail.com>
2022-12-29 11:12:58 +0700

    remove unused files

--- packages/contract/contracts/AAWalletV1.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-import "@account-abstraction/contracts/core/BaseAccount.sol";
-import "./interfaces/IAAWallet.sol";
-
-contract AAWalletV1 is IAAWallet {}
